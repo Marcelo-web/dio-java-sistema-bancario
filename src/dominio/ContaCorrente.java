0994c081fb460378035e9ba17f2137ef1b26bd32
Marcelo <marestdesenv@gmail.com>
2024-06-29 20:19:36 -0300

    evolucao inicial dos conceitos de abstracao

---
+++ src/dominio/ContaCorrente.java
@@ -0,0 +1,17 @@
+package dominio;
+
+public class ContaCorrente {
+
+	public int agencia;
+	public int numero;
+	public double saldo;
+
+	public void sacar() {
+	}
+
+	public void depositar() {
+	}
+
+	public void transferir() {
+	}
+}
---
+++ src/dominio/ContaPoupanca.java
@@ -0,0 +1,17 @@
+package dominio;
+
+public class ContaPoupanca {
+
+	public int agencia;
+	public int numero;
+	public double saldo;
+
+	public void sacar() {
+	}
+
+	public void depositar() {
+	}
+
+	public void transferir() {
+	}
+}
